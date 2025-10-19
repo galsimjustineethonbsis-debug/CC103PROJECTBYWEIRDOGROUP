e31129e1e3f9f16cf54042829c4e54be9751b018
EbiEbiEbiEbiEbiEbi <bestdudeblob@gmail.com>
2025-10-19 10:08:11 +0800

    added computeparkingfee method but both inputvehicleinfo and computeparking fee still needs lost ticket and discount system

--- Main.java
+++ Main.java
@@ -5,7 +5,8 @@ public class Main {
     static String vehicleType,plateNum;
     static int carCount=0,motorCount=0,truckCount=0;
     static int timeIn, timeOut;
-    static int timeInHr,timeInMin,timeOutHr,timeOutMin;
+    static int timeInHr, timeInMin, timeOutHr, timeOutMin;
+    static double total;
 
     static String border = "----------------------------------------";
     
@@ -101,62 +102,104 @@ public class Main {
                 carCount++;
                 else if(vehicleType.equalsIgnoreCase("motorcycle"))
                 motorCount++;
-                else if(vehicleType.equalsIgnoreCase("truck"))
-                truckCount++;
+                else if(vehicleType.equalsIgnoreCase("truck")|| vehicleType.equalsIgnoreCase("suv"))
+                truckCount++;                
                 else{
-                    System.out.print(border +"\nINVALID INPUT! ONLY (Car, Motorcycle,Truck) IS ACCEPTED\n"+ border);
+                    System.out.print(border +"\nINVALID INPUT! ONLY (Car, Motorcycle,Truck, SUV) IS ACCEPTED\n"+ border);
                 }
 
-            }while(!((vehicleType.equalsIgnoreCase("car")) ||(vehicleType.equalsIgnoreCase("motorcycle")) || (vehicleType.equalsIgnoreCase("truck"))));
+            }while(!((vehicleType.equalsIgnoreCase("car")) ||(vehicleType.equalsIgnoreCase("motorcycle")) || (vehicleType.equalsIgnoreCase("truck") || (vehicleType.equalsIgnoreCase("suv")))));
+            //line 110 validates input
+            System.out.print("\nEnter vehicle Plate Number with NO SPACES(eg. abc-1234): ");
+            plateNum  = inputVehicle.next();
 
-            System.out.print("\nEnter vehicle Plate Number(xxx-xxxx): ");
-            vehicleType = inputVehicle.next();
-
-      
-            
-
-
-    
-                do{
+            System.out.println("\nTIME IN");
+            do{
                     
-                    System.out.print("\nEnter vehicle time IN  (0000(12:00am)-2359(11:59pm) with NO colons \":\"): ");
-                    timeIn = inputVehicle.nextInt();
+                System.out.print("\nEnter vehicle time IN (0000(12:00am)-2359(11:59pm) with NO colons \":\"): ");
+                timeIn = inputVehicle.nextInt();
 
-                    if((timeIn<0 || timeIn>2400) || (timeIn/100>=24 || timeIn%100>=60)){
-                        System.out.println("\nInvalid input! time IN must be 0000-2359 ONLY");
-                    }
+                if((timeIn<0 || timeIn>2400) || (timeIn/100>=24 || timeIn%100>=60)){// time/100 = first 2 digits and time%100 = last 2 digits
+                    System.out.println("\nInvalid input! time IN must be 0000-2359 ONLY");
+                }
 
-                }while((timeIn<0 || timeIn>2400) ||(timeIn/100>=24 || timeIn%100>=60));
-                System.out.println(border);
-                do{
+            }while((timeIn<0 || timeIn>2400) ||(timeIn/100>=24 || timeIn%100>=60));
+            
+            System.out.println("\nTIME OUT");
+            do{
 
-                        System.out.print( "Enter vehicle time OUT (0000(12:00am)-2359(11:59pm) with NO colons \":\"): ");
-                        timeOut = inputVehicle.nextInt(); 
+                System.out.print( "\nEnter vehicle time OUT (0000(12:00am)-2359(11:59pm) with NO colons \":\"): ");
+                timeOut = inputVehicle.nextInt(); 
 
+                if((timeOut<timeIn || timeOut>2400) || (timeOut/100>=24 || timeOut%100>=60)){
+                    System.out.println("\nInvalid input! time OUT must be 0000-2359 ONLY and can NOT be LESS THAN  timme IN");
+                }
                 
-                }while(timeOut<timeIn || timeIn>2359 || (timeOut/100>=24 || timeOut%100>=60 ));
-
+            }while(timeOut<timeIn || timeOut>2359 || (timeOut/100>=24 || timeOut%100>=60 ));
 
 
             
             do{
-            System.out.print("\ntry again? (Y/N): ");
-            confirm = inputVehicle.next();
-            if(confirm.equalsIgnoreCase("n"))
-                status = false;
+                System.out.print("\ntry again? (Y/N): ");
+                confirm = inputVehicle.next();
+                if(confirm.equalsIgnoreCase("n"))
+                    status = false;                    
+                
+                else if(confirm.equalsIgnoreCase("y"))
+                    status = true;
+                else{
+                    System.out.println("Invalid input please try again.\n");
+                }
+            }while(!(confirm.equalsIgnoreCase("y")|| confirm.equalsIgnoreCase("n")));
             
-            else if(confirm.equalsIgnoreCase("y"))
-                status = true;
-            else{
-                System.out.println("Invalid input please try again.\n");
-            }
-        }while(!(confirm.equalsIgnoreCase("y")|| confirm.equalsIgnoreCase("n")));
-            
-            
+            timeInHr = timeIn / 100;
+            timeInMin = timeIn % 100;
+            timeOutHr = timeOut / 100;
+            timeOutMin = timeOut % 100;
 
-        }while(status = true);
+            System.out.println(timeInHr + ":" + timeInMin);
+            
+            System.out.println(ComputeParkngFee());
+
+        }while(status == true);
 
     }//inputs vehicle info
+
+    static double ComputeParkngFee(){
+        int totalMinutesIN = (timeInHr*60) + timeInMin, totalMinutesOUT = (timeOutHr*60) + timeOutMin, timeDiff = (totalMinutesOUT - totalMinutesIN);
+        int rate=0;
+        double fee=0;
+
+        
+        if(vehicleType.equalsIgnoreCase("car"))
+            rate = 20;
+        else if(vehicleType.equalsIgnoreCase("motorcycle"))
+            rate = 10;
+        else if(vehicleType.equalsIgnoreCase("truck") || vehicleType.equalsIgnoreCase("suv")){
+            rate = 30;
+        }
+
+        int firstHour = rate*2;
+
+        if(totalMinutesIN<=30){
+            fee = 0;
+        }
+        else{
+            if((timeDiff/60)%60 != 0){
+                timeDiff += 60;
+            }
+            fee = firstHour + (((timeDiff/60)- 1 )* rate);
+        }
+
+        total+=fee;
+        return fee;        
+
+    }//computes parking fee
+
+    
+
+
+
 
 
 
